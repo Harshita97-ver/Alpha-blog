035dbd2c7dee6082fb1d25f2540d71fcd196d2f4
Harshita Verma <vermaharshita9741@gmail.com>
2020-07-14 02:37:35 +0530

    user side is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
 
+    validates :username, 
+                        presence: true,
+                        uniqueness: {case_sensitive: false}, 
+                        length: {minimum: 3}
+    validates :username,
+                         uniqueness:{ case_sensitive: false },
+                         length: {maximum: 25}
+    validates :email, 
+                        presence: true,
+                        uniqueness:{ case_sensitive: false }, 
+                        length: {minimum: 105}
 end
