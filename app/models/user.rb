20cd1e8f0a58a757a531a19d3737dd4b7991474b
Harshita Verma <vermaharshita9741@gmail.com>
2020-07-15 01:41:47 +0530

    user side  connected

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,19 @@ class User < ApplicationRecord
                         presence: true,
                         uniqueness: {case_sensitive: false}, 
                         length: {minimum: 3}
-    validates :username,
+        VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i     
+     validates :username,
                          uniqueness:{ case_sensitive: false },
                          length: {maximum: 25}
-    validates :email, 
-                        presence: true,
-                        uniqueness:{ case_sensitive: false }, 
-                        length: {minimum: 105}
+   # validates :email, 
+    #                    presence: true,
+     #                   uniqueness:{ case_sensitive: false }, 
+      #                  length: {minimum: 105}
+       #                 format: { with:VALID_EMAIL_REGEX}
+
+validates :email, presence: true, uniqueness: { case_sensitive: false },length: { in: 6..150 },
+    format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "Not proper email"}
+    #has_secure_password
+
+
 end
