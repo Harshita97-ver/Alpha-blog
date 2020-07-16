b4628307b2aff9882e9d756f557b2bdfc8b60152
Harshita Verma <vermaharshita9741@gmail.com>
2020-07-17 02:05:17 +0530

    Association done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,22 @@
 class User < ApplicationRecord
 
+    has_many :articles
     validates :username, 
                         presence: true,
                         uniqueness: {case_sensitive: false}, 
                         length: {minimum: 3}
-        VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i     
+      
      validates :username,
                          uniqueness:{ case_sensitive: false },
                          length: {maximum: 25}
-   # validates :email, 
-    #                    presence: true,
-     #                   uniqueness:{ case_sensitive: false }, 
-      #                  length: {minimum: 105}
-       #                 format: { with:VALID_EMAIL_REGEX}
+  
 
-validates :email, presence: true, uniqueness: { case_sensitive: false },length: { in: 6..150 },
-    format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "Not proper email"}
-    #has_secure_password
+    validates :email,
+                         presence: true, 
+                         uniqueness: { case_sensitive: false },
+                         length: { in: 6..150 },
+                         format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "Not proper email"}
+  
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_12_210010) do
+ActiveRecord::Schema.define(version: 2020_07_15_191658) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
