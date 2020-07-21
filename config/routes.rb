2e24be3f7330e5fed7ae914fd580742a8f13411f
Harshita Verma <vermaharshita9741@gmail.com>
2020-07-21 17:35:31 +0530

    userside update n edit complete

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
     resources :articles
     get 'signup', to:'users#new'
     post 'users', to: 'users#create'
+#    get 'edit', to:'users#update'
+    resources :users
   end
