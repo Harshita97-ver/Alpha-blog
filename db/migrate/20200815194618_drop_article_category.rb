5bfb9481cbfffd50346880bb573a55af0f18faad
Harshita Verma <vermaharshita9741@gmail.com>
2020-08-18 00:36:58 +0530

    making app production ready

--- db/migrate/20200815194618_drop_article_category.rb
+++
@@ -1,4 +0,0 @@
-class DropArticleCategory < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
--- db/migrate/20200815194751_drop_create_article_categories.rb
+++
@@ -1,4 +0,0 @@
-class DropCreateArticleCategories < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
